bd3ed370f3710154e26fa12c93fad902413e02b1
MarijaK02 <karajanova289@gmail.com>
2025-01-27 00:05:37 +0100

    deleting old

--- Domasna2/food_delivery_app/lib/models/comment.dart
+++
@@ -1,9 +0,0 @@
-class Comment{
-  final String body;
-  final DateTime createdOn;
-
-  Comment({
-    required this.body,
-    required this.createdOn
-  });
-}
--- Domasna2/food_delivery_app/lib/service/restaurant_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:food_delivery_app/models/restaurant.dart';
-
-import '../data/restaurant_data.dart';
-
-class RestaurantService{
-  List<Restaurant> getRestaurants() {
-    return rawRestaurantData.map((json) => Restaurant.fromJson(json)).toList();
-  }
-}
